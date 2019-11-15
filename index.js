e306abe6c37bb883cfd164cea8ee1d9cc4a9b7fa
Tori Fife <torifife@gmail.com>
2019-11-14 17:05:50 -0700

    make it better

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 
-const PORT = 5000;
+var PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.urlencoded())
